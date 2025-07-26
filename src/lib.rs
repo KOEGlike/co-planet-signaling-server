d822b4d1a5d442a64721a6105cc3422e9848cf80
Balázs Marcell <KoegLike@proton.me>
2025-07-26 19:01:03 +0300

    Update dependencies and refactor code structure for improved clarity and functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,20 @@
-use std::{
-    prelude::*,
-    sync::{Arc},
-    vec,
-};
+use std::{prelude::*, sync::Arc, vec};
 
 use axum::{
     extract::{
         State,
-        ws::{WebSocket, WebSocketUpgrade},
+        ws::{Message, Utf8Bytes, WebSocket, WebSocketUpgrade},
     },
     response::{IntoResponse, Response},
 };
 
 use serde::{Deserialize, Serialize};
 
-use tokio::sync::{broadcast,Mutex};
+use tokio::sync::{Mutex, broadcast};
 
 use thiserror::Error;
+
+use futures_util::{sink::SinkExt, stream::StreamExt};
 
 #[derive(Error, Debug)]
 enum Error {}
@@ -27,8 +25,7 @@ pub struct Peer {
 }
 
 pub struct Lobby {
-    name: String,
-    id: i128,
+    id: String,
     mesh: bool,
     peers: Vec<i128>,
     channel: broadcast::Sender<ResponseType>,
@@ -42,25 +39,41 @@ pub struct AppState {
 
 pub type AppStateWrapped = Arc<Mutex<AppState>>;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(tag = "type")]
 enum RequestType {
     Join { lobby_id: Option<i128> },
     Relay { id: i128, message: RelayMessage },
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(tag = "type")]
 enum ResponseType {
-    ID { id: i128, mesh: bool },
-    PeerConnect { id: i128 },
-    PeerDisconnect { id: i128 },
-    Relay { id: i128, message: RelayMessage },
-    SuccessfulJoin { id: i128 },
-    Error { code: i16, message: String },
+    ID {
+        id: i128,
+        lobby_id: i128,
+        mesh: bool,
+    },
+    PeerConnect {
+        id: i128,
+    },
+    PeerDisconnect {
+        id: i128,
+    },
+    Relay {
+        id: i128,
+        message: RelayMessage,
+    },
+    SuccessfulJoin {
+        id: i128,
+    },
+    Error {
+        code: i16,
+        message: String,
+    },
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(tag = "type")]
 enum RelayMessage {
     Offer(String),
@@ -77,17 +90,41 @@ pub async fn ws_handler(ws: WebSocketUpgrade, State(state): State<AppStateWrappe
 }
 
 async fn handle_socket(mut socket: WebSocket, state: AppStateWrapped) {
-    while let Some(msg) = socket.recv().await {
-        let msg = if let Ok(msg) = msg {
-            msg
-        } else {
-            // client disconnected
-            return;
-        };
+    let (mut sender, mut receiver) = socket.split();
+    
+    // while let Some(msg) = socket.recv().await {
+    //     let msg = if let Ok(msg) = msg {
+    //         msg
+    //     } else {
+    //         // client disconnected
+    //         return;
+    //     };
 
-        if socket.send(msg).await.is_err() {
-            // client disconnected
-            return;
-        }
-    }
+    //     if socket.send(msg).await.is_err() {
+    //         // client disconnected
+    //         return;
+    //     }
+    // }
+}
+
+async fn write(){
+
+}
+
+async fn create_lobby(mesh: bool, state: AppStateWrapped) -> String {
+    let (send, _) = broadcast::channel::<ResponseType>(32);
+    let id = cuid2::CuidConstructor::new().with_length(8).create_id();
+
+    let mut state = (*state).lock().await;
+
+    let lobby = Lobby {
+        id: id.clone(),
+        mesh,
+        peers: vec![],
+        channel: send,
+    };
+
+    state.lobbies.push(lobby);
+
+    id
 }
